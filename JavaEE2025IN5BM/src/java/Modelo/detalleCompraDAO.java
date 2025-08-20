d4f24f43e80909f5539ae5ec4a5207bcb1675338
cmartinez-2024032 <cmartinez-2024032@kinal.edu.gt>
2025-08-20 11:11:02 -0600

    Feat: Agregar el Actualizar, Eliminar y Agregar de la Entidad Detalle Compras

--- JavaEE2025IN5BM/src/java/Modelo/detalleCompraDAO.java
+++ JavaEE2025IN5BM/src/java/Modelo/detalleCompraDAO.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class detalleCompraDAO {
+public class DetalleCompraDAO {
 
     Conexion cn = new Conexion();
     Connection con;
@@ -15,15 +15,15 @@ public class detalleCompraDAO {
     ResultSet rs;
     int resp;
 
-    public List<detalleCompra> listar() {
-        List<detalleCompra> listaDetalle = new ArrayList<>();
+    public List<DetalleCompra> listar() {
+        List<DetalleCompra> listaDetalle = new ArrayList<>();
         String sql = "call sp_listarDetalleCompras()";
         try {
             con = cn.Conexion();
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
             while (rs.next()) {
-                detalleCompra dc = new detalleCompra();
+                DetalleCompra dc = new DetalleCompra();
                 dc.setCodigoDetalleCompra(rs.getInt(1));
                 dc.setCantidad(rs.getInt(2));
                 dc.setPrecioUnitario(rs.getDouble(3));
@@ -37,7 +37,7 @@ public class detalleCompraDAO {
         return listaDetalle;
     }
 
-    public int agregar(detalleCompra dc) {
+    public int agregar(DetalleCompra dc) {
         String sql = "call sp_agregarDetalleCompra(?,?,?,?)";
         try {
             con = cn.Conexion();
@@ -52,4 +52,59 @@ public class detalleCompraDAO {
         }
         return resp;
     }
+
+   public DetalleCompra listarCodigoPorDetalle(int id) {
+    DetalleCompra dc = new DetalleCompra();
+    String sql = "SELECT * FROM DetalleCompras WHERE codigoDetalleCompra = " + id;
+    try {
+        con = cn.Conexion();
+        ps = con.prepareStatement(sql);
+        rs = ps.executeQuery();
+        if (rs.next()) {
+            dc.setCodigoDetalleCompra(rs.getInt(1));
+            dc.setCantidad(rs.getInt(2));          
+            dc.setPrecioUnitario(rs.getDouble(3));
+            dc.setCodigoCompra(rs.getInt(4));
+            dc.setCodigoProducto(rs.getInt(5));
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return dc;
+}
+
+public int actualizar(DetalleCompra dc) {
+    String sql = "CALL sp_editarDetalleCompra(?,?,?,?,?)";
+    try {
+        con = cn.Conexion();
+        ps = con.prepareStatement(sql);
+        ps.setInt(1, dc.getCodigoDetalleCompra());
+        ps.setInt(2, dc.getCantidad());
+        ps.setDouble(3, dc.getPrecioUnitario());
+        ps.setInt(4, dc.getCodigoCompra());
+        ps.setInt(5, dc.getCodigoProducto());
+        ps.executeUpdate();
+       
+    } catch (Exception e) {
+        e.printStackTrace();
+
+    }
+return resp;
+}
+
+public void eliminar(int codigoDetalle) {
+    String sql = "CALL sp_eliminarDetalleCompra(?)";
+    try {
+        con = cn.Conexion();
+        ps = con.prepareStatement(sql);
+        ps.setInt(1, codigoDetalle);
+        ps.executeUpdate();
+        
+    } catch (Exception e) {
+        e.printStackTrace();
+        
+    }
+    
+}
+
 }
