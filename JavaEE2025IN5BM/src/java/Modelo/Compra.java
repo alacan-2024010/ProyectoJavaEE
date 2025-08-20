8d2e2bca59bcb75888150c66c83f60a7ee39f0e6
iperez-2024003 <iperez-2024003@kinal.edu.gt>
2025-08-20 10:48:33 -0600

    Feat:Agregar CRUD completo de la entidad Compras

--- JavaEE2025IN5BM/src/java/Modelo/Compra.java
+++ JavaEE2025IN5BM/src/java/Modelo/Compra.java
@@ -1,91 +1,59 @@
 package Modelo;
 
-import java.math.BigDecimal;
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Scanner;
-
-@Entity
-@Table(name = "Compras")
 public class Compra {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int codigoCompra;
-
-    @Column
-    private LocalDateTime fechaCompra;
-
-    @Column(precision = 10, scale = 2)
-    private BigDecimal total;
-
-    @Column
+    private String fechaCompra;
+    private double total;
     private int codigoProveedor;
-
-    @Column
     private int codigoEmpleado;
-
+ 
     public Compra() {
     }
-
-    public Compra(LocalDateTime fechaCompra, BigDecimal total, int codigoProveedor, int codigoEmpleado) {
+    
+    public Compra(String fechaCompra, double total, int codigoProveedor, int codigoEmpleado) {
         this.fechaCompra = fechaCompra;
         this.total = total;
         this.codigoProveedor = codigoProveedor;
         this.codigoEmpleado = codigoEmpleado;
     }
-
+    
     public int getCodigoCompra() {
         return codigoCompra;
     }
-
+    
     public void setCodigoCompra(int codigoCompra) {
         this.codigoCompra = codigoCompra;
     }
-
-    public LocalDateTime getFechaCompra() {
+    
+    public String getFechaCompra() {
         return fechaCompra;
     }
-
-    public void setFechaCompra(LocalDateTime fechaCompra) {
+    
+    public void setFechaCompra(String fechaCompra) {
         this.fechaCompra = fechaCompra;
     }
-
-    public BigDecimal getTotal() {
+    
+    public double getTotal() {
         return total;
     }
-
-    public void setTotal(BigDecimal total) {
+    
+    public void setTotal(double total) {
         this.total = total;
     }
-
+    
     public int getCodigoProveedor() {
         return codigoProveedor;
     }
-
+    
     public void setCodigoProveedor(int codigoProveedor) {
         this.codigoProveedor = codigoProveedor;
     }
-
+    
     public int getCodigoEmpleado() {
         return codigoEmpleado;
     }
-
+    
     public void setCodigoEmpleado(int codigoEmpleado) {
         this.codigoEmpleado = codigoEmpleado;
-    }
-
-    @Override
-    public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return "Compra{" +
-                "codigoCompra=" + codigoCompra +
-                ", fechaCompra=" + (fechaCompra != null ? fechaCompra.format(formatter) : "null") +
-                ", total=" + total +
-                ", codigoProveedor=" + codigoProveedor +
-                ", codigoEmpleado=" + codigoEmpleado +
-                '}';
     }
 }
